1591633f958eb178ed4a985a97a2f9b8aba9f882
charlie-barnes <charlie@cucaera.co.uk>
2015-11-19 13:53:51 +0000

    Updated version

--- dipper-stda.py
+++ dipper-stda.py
@@ -17,7 +17,7 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__version__ = "1.0a7"
+__version__ = "1.1.0"
 
 import gobject
 import gtk
