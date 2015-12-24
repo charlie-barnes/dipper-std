34323b6fa5f774475e63acb09e71d1b0cea8ae9e
charlie-barnes <charlie@cucaera.co.uk>
2015-12-24 15:40:03 +0000

    Added vc restriction to contributor code

--- atlas.py
+++ atlas.py
@@ -504,7 +504,8 @@ class Atlas(gobject.GObject):
         contrib_data = {}
 
         self.dataset.cursor.execute('SELECT DISTINCT(data.recorder) \
-                                    FROM data')
+                                     WHERE ' + vcs_sql ' \
+                                     FROM data')
 
         recorder_data = self.dataset.cursor.fetchall()
 
@@ -541,6 +542,7 @@ class Atlas(gobject.GObject):
                         contrib_data[name.strip()] = ''.join(initials)
 
         self.dataset.cursor.execute('SELECT DISTINCT(data.determiner) \
+                                     WHERE ' + vcs_sql ' \
                                      FROM data')
 
         determiner_data = self.dataset.cursor.fetchall()
